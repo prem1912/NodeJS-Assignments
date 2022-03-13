ef4dfe612484fb9dc51e9ed2fe9afafe91653bcd
prem1912 <premeditz121@gmail.com>
2022-03-13 10:46:35 +0530

    assignment-3

---
+++ assignments/assignment_3/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const faker = require("faker");
+const app = express();
+
+app.use(bodyParser());
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+var users = [];
+for (let i = 0; i<5; i++){
+    users.push({
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        age: i+24,
+        city: faker.address.city(),
+        profession: faker.name.jobTitle()
+    })
+}
+app.get("/", (req, res) =>{
+    res.render("index.ejs", {users});
+})
+app.get("/form", (req, res) =>{
+    res.render("form.ejs");
+})
+app.post("/user/add", (req, res) =>{
+    users.push({
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age,
+        city: req.body.city,
+        profession: req.body.profession
+    })
+    res.redirect("/");
+})
+app.listen(3000, ()=> console.log("Server is Connected...."));
